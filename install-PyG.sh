ebdb26e118033d7678cd14364641bb9fa4e7ec4d
Yuke Wang <505008605@qq.com>
2019-11-12 18:06:58 -0800

    add install script

---
+++ install-PyG.sh
@@ -0,0 +1,7 @@
+conda install pytorch
+conda install torchvision
+pip install  torch-scatter 
+pip install  torch-sparse 
+pip install  torch-cluster 
+pip install  torch-spline-conv 
+pip install  torch-geometric 
